44802f3074c6d9303a763ac1240886b4869997b7
Prizerl <152689419+Prizerl@users.noreply.github.com>
2024-05-26 22:03:01 +0900

    시간(일, 시, 분)

--- Assets/Script/TimerScript.cs
+++ Assets/Script/TimerScript.cs
@@ -6,32 +6,32 @@ using UnityEngine.UI;
 public class TimerScript : MonoBehaviour
 {
     public Text TimeText;
-    float sec;
-    int min;
+    int day = 0;
+    int hour = 0;
+    int min = 0;
 
     //외부 스크립트에서 수정 못하게할때
     //[SerializeField]
 
     void Start()
     {
-        //시간 조절
-        Time.timeScale = 0.05f;
-    }
-
-    void Update()
-    {
-        RunTime();
+        // 게임의 하루 = 현실의 6분,     게임의 1시간 = 현실의 15초,        min += 4
+        InvokeRepeating("RunTime", 1, 1);
     }
 
     void RunTime()
     {
-        sec += Time.time;
-        TimeText.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
-
-        if ((int)sec > 59)
+        min += 4;
+        if (min >= 60)
         {
-            sec = 0;
-            min++;
+            min = 0;
+            hour++;
         }
+        if (hour >= 24){
+            hour = 0;
+            day++;
+        }
+
+        TimeText.text = string.Format("{0}일 {1:D2}:{2:D2}", day, hour, min);
     }
 }
